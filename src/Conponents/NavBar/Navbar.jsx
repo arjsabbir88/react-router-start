31929c8b9504dfd793f83f9e1626bb3affe23bb3
sabbir88 <arjsabbir88@gmail.com>
2025-04-15 13:37:49 +0600

    added to github

---
+++ src/Conponents/NavBar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        {/* <ul className='flex'>
+            <li className= "ml-6"><a href="/">Home</a></li>
+            <li className= "ml-6"><a href="/">About</a></li>
+            <li className= "ml-6"><a href="/">Contract</a></li>
+        </ul> */}
+    </div>
+  )
+}
